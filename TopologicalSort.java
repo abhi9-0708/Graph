891ee0af5544952a0441742a73d1c011c5150017
abhi9-0708 <147010966+abhi9-0708@users.noreply.github.com>
2023-10-23 09:59:23 +0530

    Add files via upload

---
+++ TopologicalSort.java
@@ -0,0 +1,16 @@
+public class TopologicalSort {
+    public static void main(String args[]){
+        ArrayList graph[]=new ArrayList[V];
+    }
+    
+}
+class Edge{
+    int src;
+    int dest;
+    int wt;
+    public Edge(int src,int dest,int wt){
+        this.src=src;
+        this.dest=dest;
+        this.wt=wt;
+    }
+}
