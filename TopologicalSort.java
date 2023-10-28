338ee2bc65dc39333fec096bee40871078db4acf
Abhinav Kumar <147010966+abhi9-0708@users.noreply.github.com>
2023-10-28 12:34:57 +0530

    Add files via upload

--- TopologicalSort.java
+++ TopologicalSort.java
@@ -1,8 +1,41 @@
+import java.util.*;
 public class TopologicalSort {
-    public static void main(String args[]){
-        ArrayList graph[]=new ArrayList[V];
+    public static void topSort(ArrayList <Edge>graph[]){
+        boolean vis[]=new boolean[graph.length];
+        Stack<Integer> s=new Stack<>();
+        for(int i=0;i<graph.length;i++){
+            if(vis[i]!=true){
+                topSortUtil(graph,i,s,vis);
+            }
+        } 
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
     }
-    
+    public static void topSortUtil(ArrayList<Edge>graph[],int curr,Stack s,boolean vis[]){
+        vis[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            if(vis[e.dest]!=true){
+                topSortUtil(graph,e.dest,s,vis);
+            }
+        }
+        s.push(curr);
+    }
+    public static void main(String args[]){
+        int V=6;
+        ArrayList<Edge>graph[]=new ArrayList[V];
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<>();
+        }
+        graph[2].add(new Edge(2,3,1));
+        graph[3].add(new Edge(3,1,1));
+        graph[4].add(new Edge(4,1,1));
+        graph[4].add(new Edge(4,0,1));
+        graph[5].add(new Edge(5,2,1));
+        graph[5].add(new Edge(5,0,1));
+        topSort(graph);        
+    }  
 }
 class Edge{
     int src;
